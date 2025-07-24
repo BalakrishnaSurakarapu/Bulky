e6f54b22689e8a35123cc54429bff480d1a925e9
BaluGoud <balugoud508@gmail.com>
2025-07-24 13:17:39 +0530

    changes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,7 +13,7 @@ namespace BulkyWeb.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+       public IActionResult Index()
         {
             return View();
         }
